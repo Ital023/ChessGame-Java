823d6cb56930bd3aa555b25be187b94ffb2b126f
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-05 20:28:15 -0300

    add class board and piece

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(3,5);
+        Board board = new Board(8,8);
 
-        System.out.println(position);
     }
 }
